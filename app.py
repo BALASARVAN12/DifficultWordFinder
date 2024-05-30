eb5f7ef395fd7ca525f0fdfb64b13b33d3ac84f5
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2024-05-30 19:49:57 +0530

    code updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from findwords import find_difficult_words
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def index():
     return render_template("index.html", difficult_words=difficult_words)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
